cdb84b2337ea5a9fe40d9492b1d9f36e323fa4ad
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-12-19 17:17:10 +0100

    dynarray: removed non-initializing constructor, added comparison operators == and !=

--- xo/container/dynarray.h
+++ xo/container/dynarray.h
@@ -13,8 +13,7 @@ namespace xo
 		typedef const T* const_iterator;
 
 		dynarray() : data_( nullptr ), end_( nullptr ) {}
-		dynarray( size_t n ) : data_( new T[ n ] ), end_( data_.get() + n ) {}
-		dynarray( size_t n, const T& v ) : data_( new T[ n ] ), end_( data_.get() + n ) { assign( v ); }
+		dynarray( size_t n, const T& v = T() ) : data_( new T[ n ] ), end_( data_.get() + n ) { assign( v ); }
 		dynarray( const dynarray& o ) : data_( new T[ o.size() ] ), end_( data_.get() + o.size() ) { std::copy( o.begin(), o.end(), begin() ); }
 		dynarray( dynarray&& o ) : data_( std::move( o.data_ ) ), end_( o.end_ ) {}
 		dynarray<T>& operator=( const dynarray& o ) { data_ = u_ptr< T[] >( new T[ o.size() ] ); end_ = data_.get() + o.size(); std::copy( o.begin(), o.end(), begin() ); return *this; }
@@ -38,6 +37,8 @@ namespace xo
 		size_t size() const { return end() - begin(); }
 		void assign( const T& v ) { std::fill( begin(), end(), v ); }
 
+		void resize( size_t n, const T& v = T() ) { data_.reset( new T[ n ] ); end_ = data_.get() + n; assign( v ); }
+
 		T* data() { return data_.get(); }
 		const T* data() const { return data_.get(); }
 
@@ -45,4 +46,19 @@ namespace xo
 		u_ptr< T[] > data_;
 		T* end_;
 	};
+
+	template< typename T >
+	bool operator==( const dynarray< T >& v1, const dynarray< T >& v2 ) {
+		if ( v1.size() == v2.size() ) {
+			for ( index_t i = 0; i < v1.size(); ++i )
+				if ( v1[ i ] != v2[ i ] )
+					return false;
+			return true;
+		}
+		else return false;
+	}
+
+	template< typename T >
+	bool operator!=( const dynarray< T >& v1, const dynarray< T >& v2 )
+	{ return !( v1 == v2 ); }
 }
