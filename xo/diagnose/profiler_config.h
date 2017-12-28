6aabf3e43bd26c727c09f589daf91e8bff2f8c0d
Thomas Geijtenbeek <tgeijten@gmail.com>
2017-12-28 17:38:55 +0100

    FIX: profiler include

--- xo/diagnose/profiler_config.h
+++ xo/diagnose/profiler_config.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include "xo/system/log.h"
+
 #ifdef XO_ENABLE_PROFILER
-#	include "xo/system/profiler.h"
+#	include "xo/diagnose/profiler.h"
 #	define XO_PROFILE_FUNCTION ::xo::scoped_section_profiler _PROFILE_SECTION_( __FUNCTION__ )
 #	define XO_PROFILE_SCOPE( scope_name_arg ) ::xo::scoped_section_profiler _PROFILE_SECTION_( scope_name_arg )
 inline void xo_profiler_reset() { ::xo::profiler::instance().reset(); }
 inline void xo_profiler_log_report( ::xo::log::level l ) { ::xo::log::message( l, ::xo::profiler::instance().report() ); }
 #else
-#	define XO_PROFILE_FUNCTION
+#	define XO_PROFILE_FUNCTION 
 #	define XO_PROFILE_SCOPE( scope_name_arg )
 inline void xo_profiler_reset() {}
 inline void xo_profiler_log_report( ::xo::log::level l ) {}
