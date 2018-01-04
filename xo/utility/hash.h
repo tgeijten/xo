9d407ca3920d6c2697927abaa91c224787afae55
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-01-04 16:18:11 +0100

    hash inline fix

--- xo/utility/hash.h
+++ xo/utility/hash.h
@@ -16,19 +16,15 @@ namespace xo
 	constexpr hash_t fnv1a_64_basis = 0xCBF29CE484222325ull;
 
 	constexpr hash_t hash_constexpr( char const* str, hash_t last_value = fnv1a_64_basis )
-	{
-		return *str ? hash_constexpr( str + 1, ( *str ^ last_value ) * fnv1a_64_prime ) : last_value;
-	}
+	{ return *str ? hash_constexpr( str + 1, ( *str ^ last_value ) * fnv1a_64_prime ) : last_value; }
 
-	hash_t hash( char const* str )
-	{
+	inline hash_t hash( char const* str ) {
 		hash_t ret = fnv1a_64_basis;
 		while ( *str ) { ret ^= *str; ret *= fnv1a_64_prime; ++str;	}
 		return ret;
 	}
 
-	hash_t hash( const std::string& str ) { return hash( str.c_str() ); }
+	inline hash_t hash( const std::string& str ) { return hash( str.c_str() ); }
 }
 
 constexpr unsigned long long operator""_hash( char const* p, size_t ) { return xo::hash_constexpr( p ); }
-
