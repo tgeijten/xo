40e4223d5904b3534c51cadba43010054a617725
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-05-18 17:34:05 +0200

    NEW: handle, for type safe indices / other handle types.

---
+++ xo/utility/handle.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "xo/numerical/math.h"
+#include "xo/string/string_tools.h"
+
+namespace xo
+{
+	template< typename ID, typename T = uint32_t >
+	struct handle
+	{
+		handle() : value( T( -1 ) ) {}
+		explicit handle( T v ) : value( v ) {}
+		explicit handle( size_t v ) : value( T( v ) ) { xo_error_if( v < constants< T >::min() || v >= constants< T >::max(), "handle cannot hold value " + to_str( v ) ); }
+
+		explicit operator bool() const { return value != T( -1 ); }
+		explicit operator T() const { return value; }
+
+		friend bool operator==( const handle a, const handle b ) { return a.value == b.value; }
+		friend bool operator!=( const handle a, const handle b ) { return a.value != b.value; }
+
+		T value;
+	};
+}
