0ae8198606d08b10d9aeec1f243b2e1f315f158d
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-06-04 17:34:07 +0200

    added read to storage_tools.h

--- xo/container/storage_tools.h
+++ xo/container/storage_tools.h
@@ -4,18 +4,31 @@
 
 namespace xo
 {
-	template< typename T > void write( storage<T>& sto, const string& str, const vec3_<T>& v ) {
+	template< typename T, typename L  > void write( storage<T, L>& sto, const string& str, const vec3_<T>& v ) {
 		auto f = sto.back();
 		f[ str + ".x" ] = v.x;
 		f[ str + ".y" ] = v.y;
 		f[ str + ".z" ] = v.z;
 	}
 
-	template< typename T > void write( storage<T>& sto, const string& str, const quat_<T>& q ) {
+	template< typename T, typename L  > void write( storage<T, L>& sto, const string& str, const quat_<T>& q ) {
 		auto f = sto.back();
 		f[ str + ".w" ] = q.w;
 		f[ str + ".x" ] = q.x;
 		f[ str + ".y" ] = q.y;
 		f[ str + ".z" ] = q.z;
+	}
+
+	template< typename T > void read( typename storage<T>::const_frame& f, const string& str, vec3_<T>& v ) {
+		v.x = f[ str + ".x" ];
+		v.y = f[ str + ".y" ];
+		v.z = f[ str + ".z" ];
+	}
+
+	template< typename T > void read( typename storage<T>::const_frame& f, const string& str, quat_<T>& q ) {
+		q.w = f[ str + ".w" ];
+		q.x = f[ str + ".x" ];
+		q.y = f[ str + ".y" ];
+		q.z = f[ str + ".z" ];
 	}
 }
