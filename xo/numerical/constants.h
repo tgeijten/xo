bdc9d1f58b0a0884d65e8650fd04b5886b746e84
Thomas Geijtenbeek <tgeijten@gmail.com>
2018-09-06 13:44:09 +0200

    U: added greater_or_equal

--- xo/numerical/constants.h
+++ xo/numerical/constants.h
@@ -27,4 +27,5 @@ namespace xo
 	// TODO: move somewhere else (not math.h because we don't want to include constants there)
 	template< typename T > bool equal( T v1, T v2, T e = const_ample_epsilon<T>() ) { return std::abs( v1 - v2 ) <= std::abs( v1 ) * e; }
 	template< typename T > bool less_or_equal( T v1, T v2, T e = const_ample_epsilon<T>() ) { return v1 < v2 || equal( v1, v2, e ); }
+	template< typename T > bool greater_or_equal( T v1, T v2, T e = const_ample_epsilon<T>() ) { return v1 > v2 || equal( v1, v2, e ); }
 }
