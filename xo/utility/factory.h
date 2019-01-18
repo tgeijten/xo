e8306ba3e8474acae12dabc34239f624871ad17c
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-01-18 14:09:39 +0100

    R: factory changes: removed operators, added scoped_factory_registrant

--- xo/utility/factory.h
+++ xo/utility/factory.h
@@ -15,37 +15,47 @@ namespace xo
 	public:
 		using create_func_t = std::function< std::unique_ptr< T >( Args... ) >;
 
-		// register class
-		template< typename U > void register_class( const std::string& name = clean_type_name<U>() ) {
+		/// register class U
+		template< typename U >
+		void register_class( const std::string& name = clean_type_name<U>() ) {
 			func_map_[ name ] = []( Args... args ) { return std::unique_ptr< T >( new U( args... ) ); };
 		}
 
-		// unregister class
+		/// unregister class type
 		void unregister_class( const std::string& type ) {
 			auto it = func_map_.find( type );
 			xo_error_if( it == func_map_.end(), "Unknown type: " + type );
 			func_map_.erase( it );
 		}
 
-		// access function
-		const create_func_t& operator[]( const std::string& type ) const {
+		/// create instance of type
+		std::unique_ptr< T > create( const std::string& type, Args... args ) {
 			auto it = func_map_.find( type );
 			xo_error_if( it == func_map_.end(), "Unknown type: " + type );
-			return it->second;
+			return it->second( args... );
 		}
 
-		// create instance
-		std::unique_ptr< T > create( const std::string& type, Args... args ) const { return ( *this )[ type ]( args... ); }
-		std::unique_ptr< T > operator()( const std::string& type, Args... args ) const { return create( type, args... ); }
-
-		// try create instance
-		std::unique_ptr< T > try_create( const std::string& type, Args... args ) const
-		{ auto it = func_map_.find( type ); return it != func_map_.end() ? it->second( args... ) : nullptr; }
+		/// try create instance of type, return nullptr if not found
+		std::unique_ptr< T > try_create( const std::string& type, Args... args ) {
+			auto it = func_map_.find( type );
+			return it != func_map_.end() ? it->second( args... ) : nullptr;
+		}
 
 		bool empty() const { return func_map_.empty(); }
 		bool has_type( const std::string& type ) const { return func_map_.find( type ) != func_map_.end(); }
 
 	private:
 		xo::flat_map< std::string, create_func_t > func_map_;
+	};
+
+	template< typename F, typename C >
+	class scoped_factory_registrant {
+	public:
+		scoped_factory_registrant( F& f, const string& name = clean_type_name<C>() ) : factory_( f ), name_( name )
+		{ factory_.register_class<C>( name_ ); }
+		~scoped_factory_registrant() { factory_.unregister_class( name_ ); }
+	private:
+		F& factory_;
+		string name_;
 	};
 }
