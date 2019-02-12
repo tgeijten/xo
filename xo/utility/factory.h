05d6c29648a6d38f6589d9dfc046d97f7a0d9172
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-02-12 18:21:54 +0100

    U: added factory::begin/end/find

--- xo/utility/factory.h
+++ xo/utility/factory.h
@@ -19,6 +19,8 @@ namespace xo
 	{
 	public:
 		using create_func_t = std::function< std::unique_ptr< T >( Args... ) >;
+		using container_t = xo::flat_map< std::string, create_func_t >;
+		using const_iterator = typename container_t::const_iterator;
 
 		/// register type U
 		template< typename U >
@@ -29,29 +31,33 @@ namespace xo
 
 		/// unregister type
 		void unregister_type( const std::string& type_id ) {
-			auto it = func_map_.find( type_id );
-			xo_error_if( it == func_map_.end(), "Unknown type: " + type_id );
+			auto it = find( type_id );
+			xo_error_if( it == end(), "Unknown type: " + type_id );
 			func_map_.erase( it );
 		}
 
 		/// create instance of type
 		std::unique_ptr< T > create( const std::string& type_id, Args... args ) {
-			auto it = func_map_.find( type_id );
-			xo_error_if( it == func_map_.end(), "Unknown type: " + type_id );
+			auto it = find( type_id );
+			xo_error_if( it == end(), "Unknown type: " + type_id );
 			return it->second( args... );
 		}
 
 		/// try create instance of type, return nullptr if not found
 		std::unique_ptr< T > try_create( const std::string& type_id, Args... args ) {
-			auto it = func_map_.find( type_id );
-			return it != func_map_.end() ? it->second( args... ) : nullptr;
+			auto it = find( type_id );
+			return it != end() ? it->second( args... ) : nullptr;
 		}
 
 		bool empty() const { return func_map_.empty(); }
 		bool has_type( const std::string& type_id ) const { return func_map_.find( type_id ) != func_map_.end(); }
 
+		const_iterator begin() const { return func_map_.begin(); }
+		const_iterator end() const { return func_map_.begin(); }
+		const_iterator find( const std::string& type ) { return func_map_.find( type ); }
+
 	private:
-		xo::flat_map< std::string, create_func_t > func_map_;
+		container_t func_map_;
 	};
 
 	template< typename T, typename F >
