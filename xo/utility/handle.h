9196065d2c8d20052923cafaf0fdf6bcd919230a
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-03-14 16:58:28 +0100

    U: handle is now copy-constructable / assignable again (as it should be), cleanup

--- xo/utility/handle.h
+++ xo/utility/handle.h
@@ -7,22 +7,21 @@ namespace xo
 	template< typename ID, typename T = uint32_t >
 	struct handle
 	{
-		const T invalid_v = T( -1 );
-
-		handle() : value( invalid_v ) {}
+		handle() : value( invalid_value ) {}
 		explicit handle( T v ) : value( v ) {}
 		template< typename U > handle( U v ) : value( T( v ) ) { xo_error_if( v < constants<T>::lowest() || v >= constants<T>::max(), "handle cannot hold value " + to_str( v ) ); }
-		handle( const handle& o ) = delete;
-		handle& operator=( const handle& o ) = delete;
-		handle( handle&& o ) { value = o.value; o.value = invalid_v; }
-		handle& operator=( handle&& o ) { value = o.value; o.value = invalid_v; return *this; }
+		handle( const handle& ) = default;
+		handle& operator=( const handle& ) = default;
+		handle( handle&& o ) { value = o.value; o.value = invalid_value; }
+		handle& operator=( handle&& o ) { value = o.value; o.value = invalid_value; return *this; }
 
-		explicit operator bool() const { return value != T( -1 ); }
+		explicit operator bool() const { return value != invalid_value; }
 		explicit operator T() const { return value; }
 
 		friend bool operator==( const handle a, const handle b ) { return a.value == b.value; }
 		friend bool operator!=( const handle a, const handle b ) { return a.value != b.value; }
 
+		static constexpr T invalid_value = T( -1 );
 		T value;
 	};
 
