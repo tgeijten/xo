3e93d7a5c901bb794c55c575e46bf1317dceff39
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-03-23 00:34:43 +0100

    U: string_cast cleanup, removed prototype for to_str(), removed container to_str()

--- xo/string/string_cast.h
+++ xo/string/string_cast.h
@@ -9,36 +9,26 @@
 
 namespace xo
 {
-	/// convert any type to string
-	template< typename T > string to_str( const T& value );
+	/// convert data from string
 	template< typename T > T from_str( const string& s, T default_value );
-
-	// specializations
 	template<> inline float from_str( const string& s, float default_value ) { char* p; auto v = std::strtof( s.c_str(), &p ); return p != s.c_str() ? v : default_value; }
-	XO_API string to_str( float value );
-
 	template<> inline double from_str( const string& s, double default_value ) { char* p; auto v = std::strtod( s.c_str(), &p ); return p != s.c_str() ? v : default_value; }
-	XO_API string to_str( double value );
-
 	template<> inline bool from_str( const string& s, bool default_value ) { return ( s == "1" || s == "true" ); }
-	XO_API string to_str( bool value );
-
 	template<> inline int from_str( const string& s, int default_value ) { char* p; auto v = (int)( std::strtol( s.c_str(), &p, 10 ) ); return p != s.c_str() ? v : default_value; }
-	XO_API string to_str( int value );
-
 	template<> inline unsigned int from_str( const string& s, unsigned int default_value ) { char* p; auto v = (unsigned int)( std::strtol( s.c_str(), &p, 10 ) ); return p != s.c_str() ? v : default_value; }
-	XO_API string to_str( unsigned int value );
-
 	template<> inline long from_str( const string& s, long default_value ) { char* p; auto v = std::strtol( s.c_str(), &p, 10 ); return p != s.c_str() ? v : default_value; }
-	XO_API string to_str( long value );
-
 	template<> inline unsigned long from_str( const string& s, unsigned long default_value ) { char* p; auto v = std::strtoul( s.c_str(), &p, 10 ); return p != s.c_str() ? v : default_value; }
-	XO_API string to_str( unsigned long value );
-
 	template<> inline const char* from_str( const string& s, const char* default_value ) { return s.c_str(); }
-	XO_API string to_str( const char* value );
-
 	template<> inline string from_str( const string& s, string default_value ) { return s; }
+
+	XO_API string to_str( float value );
+	XO_API string to_str( double value );
+	XO_API string to_str( bool value );
+	XO_API string to_str( int value );
+	XO_API string to_str( unsigned int value );
+	XO_API string to_str( long value );
+	XO_API string to_str( unsigned long value );
+	XO_API string to_str( const char* value );
 	inline string to_str( string value ) { return value; }
 
 	template< typename T, typename E = void > struct string_cast {
@@ -51,15 +41,15 @@ namespace xo
 		static string to( const T& v ) { return string_cast< int >::to( static_cast< int >( v ) ); }
 	};
 
-	/// make string out of container of elements
-	template< typename C > string to_str( const C& c, const string& delim ) {
-		string s;
-		for ( auto it = std::begin( c ); it != std::end( c ); ++it ) {
-			if ( it != c.begin() ) s += delim;
-			s += to_str( *it );
-		}
-		return s;
-	}
+	///// make string out of container of elements
+	//template< typename C > string to_str( const C& c, const string& delim ) {
+	//	string s;
+	//	for ( auto it = std::begin( c ); it != std::end( c ); ++it ) {
+	//		if ( it != c.begin() ) s += delim;
+	//		s += to_str( *it );
+	//	}
+	//	return s;
+	//}
 
 	template< typename T > string to_str( const T& value ) { return string_cast<T>::to( value ); }
 	template< typename T > T from_str( const string& s, T default_value ) { return string_cast<T>::from( s ); }
