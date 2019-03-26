6c8aca821db8817f8347bcea227a1776392f6415
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-03-26 12:07:24 +0100

    R: renamed timer.h -> timer_v1.h

--- xo/time/time.h
+++ xo/time/time.h
@@ -4,10 +4,12 @@ namespace xo
 {
 	struct time
 	{
+		time() : ns( 0 ) {}
 		time( long long nanoseconds ) : ns( nanoseconds ) {}
+
 		double seconds() const { return double( ns ) / 1e9; }
-		long long milliseconds() const { return ns / 1000000; }
-		long long microseconds() const { return ns / 1000; }
+		long long milliseconds() const { return ns / 1'000'000; }
+		long long microseconds() const { return ns / 1'000; }
 		long long nanoseconds() const { return ns; }
 
 		bool operator<( time o ) const { return ns < o.ns; }
@@ -21,9 +23,9 @@ namespace xo
 
 	inline namespace literals {
 		constexpr time operator ""_ns( long long ns ) { return time( ns ); }
-		constexpr time operator ""_ms( long long ms ) { return time( ms * 1000000 ); }
-		constexpr time operator ""_s( long long s ) { return time( s * 1000000000 ); }
-		constexpr time operator ""_m( long long m ) { return time( h * 60 * 1000000000 ); }
-		constexpr time operator ""_h( long long h ) { return time( h * 60 * 60 * 1000000000 ); }
+		constexpr time operator ""_ms( long long ms ) { return time( ms * 1'000'000 ); }
+		constexpr time operator ""_s( long long s ) { return time( s * 1'000'000'000 ); }
+		constexpr time operator ""_m( long long m ) { return time( h * 60 * 1'000'000'000 ); }
+		constexpr time operator ""_h( long long h ) { return time( h * 60 * 60 * 1'000'000'000 ); }
 	}
 }
