45517783e58a9c8b5e078e0e8fd9eeb0cedef73f
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-04-04 14:48:27 +0200

    R: removed xo_profiler_log_report

--- xo/diagnose/profiler_config.h
+++ xo/diagnose/profiler_config.h
@@ -4,16 +4,13 @@
 
 #ifdef XO_ENABLE_PROFILER
 #	include "xo/diagnose/profiler.h"
-#	include "xo/system/log.h"
 #	define XO_PROFILE_FUNCTION ::xo::scoped_section_profiler _PROFILE_SECTION_( __FUNCTION__ )
 #	define XO_PROFILE_SCOPE( scope_name_arg ) ::xo::scoped_section_profiler _PROFILE_SECTION_( scope_name_arg )
 inline void xo_profiler_reset() { ::xo::profiler::instance().reset(); }
-inline void xo_profiler_log_report( ::xo::log::level l ) { ::xo::log::message( l, ::xo::profiler::instance().report() ); }
 inline ::xo::prop_node xo_profiler_report() { return ::xo::profiler::instance().report(); }
 #else
 #	define XO_PROFILE_FUNCTION 
 #	define XO_PROFILE_SCOPE( scope_name_arg )
 inline void xo_profiler_reset() {}
-inline void xo_profiler_log_report( ::xo::log::level l ) {}
 inline const char* xo_profiler_report() { return ""; }
 #endif
