35b71e45c730df9bfcfbfcec3cce99e14ebba2c5
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-05-10 17:33:47 +0200

    F: XO_PROFILE_FUNCTION

--- xo/diagnose/profiler_config.h
+++ xo/diagnose/profiler_config.h
@@ -4,13 +4,13 @@
 
 #ifdef XO_ENABLE_PROFILER
 #	include "xo/diagnose/profiler.h"
-#	define XO_PROFILE_FUNCTION ::xo::scoped_section_profiler _PROFILE_SECTION_( __FUNCTION__ )
-#	define XO_PROFILE_SCOPE( scope_name_arg ) ::xo::scoped_section_profiler _PROFILE_SECTION_( scope_name_arg )
-inline void xo_profiler_reset() { ::xo::profiler::instance().start(); }
+#	define XO_PROFILE_FUNCTION ::xo::scope_profiler _PROFILE_SECTION_( __FUNCTION__ )
+#	define XO_PROFILE_SCOPE( scope_name_arg ) ::xo::scope_profiler _PROFILE_SECTION_( scope_name_arg )
+inline void xo_profiler_start() { ::xo::profiler::instance().start(); }
 inline ::xo::prop_node xo_profiler_report() { return ::xo::profiler::instance().report(); }
 #else
 #	define XO_PROFILE_FUNCTION 
 #	define XO_PROFILE_SCOPE( scope_name_arg )
-inline void xo_profiler_reset() {}
+inline void xo_profiler_start() {}
 inline const char* xo_profiler_report() { return ""; }
 #endif
