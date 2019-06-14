15055ada539fcfda14c8fb99c8604522617f5cf3
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-06-14 12:53:22 +0200

    N: added quat::length() and quat::squared_length()

--- xo/geometry/quat_type.h
+++ xo/geometry/quat_type.h
@@ -19,10 +19,10 @@ namespace xo
 		template< typename T2 > quat_<T>& operator=( const quat_<T2>& o ) { w = T( o.w ); x = T( o.x ); y = T( o.y ); z = T( o.z ); return *this; }
 		template< typename T2 > void set( T2 pw, T2 px, T2 py, T2 pz ) { w = T( pw ); x = T( px ); y = T( py ); z = T( pz ); }
 
-		/// conjugate
+		/// const properties
+		T length() const { return sqrt( w * w + x * x + y * y + z * z ); }
+		T squared_length() const { return w * w + x * x + y * y + z * z; }
 		quat_<T> conjugate() const { return quat_<T>( w, -x, -y, -z ); }
-
-		/// inverse
 		quat_<T> inverse() const { auto s = w * w + x * x + y * y + z * z; xo_assert( s > T( 0 ) ); auto f = T( -1 ) / s; return quat_<T>( w, f * x, f * y, f * z ); }
 
 		/// member access
