1ad830fe2fe64d967210c8505147b000d6f53e60
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-06-19 17:42:40 +0200

    C: handle cleanup

--- xo/utility/handle.h
+++ xo/utility/handle.h
@@ -4,24 +4,26 @@
 
 namespace xo
 {
-	template< typename ID, typename T = uint32_t >
+	template< typename T, typename I = uint32_t >
 	struct handle
 	{
+		using value_t = typename I;
+
 		handle() : value( invalid_value ) {}
-		explicit handle( T v ) : value( v ) {}
-		template< typename U > handle( U v ) : value( T( v ) ) { xo_error_if( v >= invalid_value, "handle cannot hold value" ); }
+		explicit handle( value_t v ) : value( v ) {}
+		template< typename U > handle( U v ) : value( value_t( v ) ) { xo_error_if( v >= invalid_value, "handle cannot hold value" ); }
 		handle( const handle& ) = default;
 		handle& operator=( const handle& ) = default;
 		handle( handle&& o ) : value( o.value ) { o.value = invalid_value; }
-		handle& operator=( handle&& o ) { T tmp = value; value = o.value; o.value = tmp; return *this; }
+		handle& operator=( handle&& o ) { value_t tmp = value; value = o.value; o.value = tmp; return *this; }
 
 		explicit operator bool() const { return value != invalid_value; }
-		explicit operator T() const { return value; }
+		explicit operator value_t() const { return value; }
 
 		friend bool operator==( const handle a, const handle b ) { return a.value == b.value; }
 		friend bool operator!=( const handle a, const handle b ) { return a.value != b.value; }
 
-		static constexpr T invalid_value = ~T(0);
-		T value;
+		static constexpr value_t invalid_value = ~value_t(0);
+		value_t value;
 	};
 }
