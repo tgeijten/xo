38bce9f4602e578003cbbdc93e02feb73ff5627a
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-06-24 13:01:35 +0200

    N: ray.h

---
+++ xo/geometry/ray.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "vec3_type.h"
+
+namespace xo
+{
+	template< typename T >
+	struct ray_
+	{
+		ray_() : p(), q() {}
+		ray_( const vec3_<T>& pos, const vec3_<T>& dir ) : pos_( pos ), dir_( dir ) {}
+
+		vec3_<T> pos_;
+		vec3_<T> dir_;
+	};
+
+	using rayf = ray_< float >;
+	using rayd = ray_< double >;
+}
