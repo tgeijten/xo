521bdf68b004eefe40e81a7fb09e223076b93b6e
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-06-24 13:00:46 +0200

    N: xo::array container that is a super-lightweight version of the (surprisingly bloated) std::array

---
+++ xo/container/array.h
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace xo
+{
+	template< typename T, size_t N >
+	class array
+	{
+	public:
+		using value_type = T;
+		using size_type = size_t;
+		using iterator = T* ;
+		using const_iterator = const T*;
+
+		T& operator[]( size_t i ) { return data_[ i ]; }
+		const T& operator[]( size_t i ) const { return data_[ i ]; }
+
+		iterator begin() { return &data_[ 0 ]; }
+		const_iterator begin() const { return &data_[ 0 ]; }
+		const_iterator cbegin() const { return &data_[ 0 ]; }
+
+		iterator end() { return &data_[ N ]; }
+		const_iterator end() const { return &data_[ N ]; }
+		const_iterator cend() const { return &data_[ N ]; }
+
+		constexpr size_t size() const { N; }
+
+		T data_[ N ];
+	};
+}
