1ebdcfebe1b233a8380e71aa095361ef9b30545a
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-06-28 15:30:21 +0200

    U: proxy now uses external proxy_data struct that can be specialized to provide the data for specific proxy types.

--- xo/utility/proxy.h
+++ xo/utility/proxy.h
@@ -6,25 +6,30 @@
 namespace xo
 {
 	template< typename T, typename I = uint32_t >
+	struct proxy_data {
+		static const T& get_value( I index ) { xo_error( "proxy_data not implemented" ); }
+		static I get_index( const T& value ) { xo_error( "proxy_data not implemented" ); }
+	};
+
+	template< typename T, typename I = uint32_t >
 	struct proxy
 	{
 		using value_t = typename T;
 		using index_t = typename I;
 
-		proxy() : index_( data_.get_or_add( value_t() ) ) {}
-		proxy( const value_t& value ) : index_( data_.get_or_add( value ) ) {}
+		proxy() : index_( typename proxy_data< T, I >::get_index( value_t() ) ) {}
+		proxy( const value_t& value ) : index_( typename proxy_data< T, I >::get_index( value ) ) {}
 
-		proxy& set( const value_t& value ) { index_ = data_.get_or_add( value ); return *this; }
+		proxy& set( const value_t& value ) { index_ = typename proxy_data< T, I >::get_index( value ); return *this; }
 		proxy& operator=( const value_t& value ) { return set( value ); }
 
-		const value_t& get() const { return data_[ index_ ]; }
+		const value_t& get() const { return typename proxy_data< T, I >::get_value( index_ ); }
 		operator const value_t&() const { return get(); }
 
 		friend bool operator==( const proxy a, const proxy b ) { return a.index_ == b.index_; }
 		friend bool operator!=( const proxy a, const proxy b ) { return a.index_ != b.index_; }
 
 	private:
-		static inline indexed_set< T, I > data_ = {};
 		index_t index_;
 	};
 
