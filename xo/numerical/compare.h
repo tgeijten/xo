adbc8997f5ff172e6f5e0cefc6b4169dc06827ed
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2019-09-03 15:31:25 +0200

    U: compare uses xo::abs()

--- xo/numerical/compare.h
+++ xo/numerical/compare.h
@@ -1,18 +1,19 @@
 #pragma once
 
 #include "constants.h"
+#include "math.h"
 
 namespace xo
 {
-	/// compare floating point variables using relaxed_epsilon
+	/// compare floating point variables using ample_epsilon
 	template< typename T > bool equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
-	{ return std::abs( v1 - v2 ) <= std::abs( v1 ) * e; }
+	{ return abs( v1 - v2 ) <= abs( v1 ) * e; }
 
-	/// compare floating point variables using relaxed_epsilon
+	/// compare floating point variables using ample_epsilon
 	template< typename T > bool less_or_equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
 	{ return v1 < v2 || equal( v1, v2, e ); }
 
-	/// compare floating point variables using relaxed_epsilon
+	/// compare floating point variables using ample_epsilon
 	template< typename T > bool greater_or_equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
 	{ return v1 > v2 || equal( v1, v2, e ); }
 }
