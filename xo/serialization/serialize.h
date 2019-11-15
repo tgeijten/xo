9c5a5b19b7972642c6e9a46dfb9864d415638e56
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2019-11-15 12:56:05 +0100

    R: register_serializer -> register_file_extension

--- xo/serialization/serialize.h
+++ xo/serialization/serialize.h
@@ -1,24 +1,22 @@
 #pragma once
 
 #include "xo/xo_types.h"
+#include "xo/string/string_type.h"
 #include "xo/serialization/prop_node_serializer.h"
 #include "xo/utility/factory.h"
-
-#include "xo/geometry/angle.h"
 #include "xo/utility/pointer_types.h"
-#include <string>
 
 namespace xo
 {
 	XO_API factory< prop_node_serializer >& get_serializer_factory();
-	XO_API u_ptr< prop_node_serializer > make_serializer( const std::string& file_type, prop_node& pn, error_code* ec = nullptr, path p = path() );
-	XO_API u_ptr< prop_node_serializer > make_serializer( const std::string& file_type, const prop_node& pn, error_code* ec = nullptr, path p = path() );
-	template< class S > void register_serializer( const std::string& extension ) { get_serializer_factory().register_type< S >( extension ); }
+	XO_API u_ptr< prop_node_serializer > make_serializer( const string& file_type, prop_node& pn, error_code* ec = nullptr, path p = path() );
+	XO_API u_ptr< prop_node_serializer > make_serializer( const string& file_type, const prop_node& pn, error_code* ec = nullptr, path p = path() );
+	template< typename S > void register_file_extension( const string& extension ) { get_serializer_factory().register_type< S >( extension ); }
 
 	XO_API prop_node load_file( const path& filename, error_code* ec = nullptr );
-	XO_API prop_node load_file( const path& filename, const std::string& file_type, error_code* ec = nullptr );
-	XO_API prop_node load_file_with_include( const path& filename, const std::string& include_directive = "INCLUDE" );
+	XO_API prop_node load_file( const path& filename, const string& file_type, error_code* ec = nullptr );
+	XO_API prop_node load_file_with_include( const path& filename, const string& include_directive = "INCLUDE" );
 
 	XO_API void save_file( const prop_node& pn, const path& filename, error_code* ec = nullptr );
-	XO_API void save_file( const prop_node& pn, const path& filename, const std::string& file_type, error_code* ec = nullptr );
+	XO_API void save_file( const prop_node& pn, const path& filename, const string& file_type, error_code* ec = nullptr );
 }
