ab80b4117a0a3538fb8442c605241afb723b9962
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2019-11-25 23:15:59 +0100

    N: quat_ now has value_type, iterator, begin(), end() and size()

--- xo/geometry/quat_type.h
+++ xo/geometry/quat_type.h
@@ -8,6 +8,10 @@ namespace xo
 	struct quat_
 	{
 	public:
+		using value_type = T;
+		using iterator = T*;
+		using const_iterator = const T*;
+
 		constexpr quat_() : w( 1 ), x( 0 ), y( 0 ), z( 0 ) {}
 		constexpr quat_( T w, T x, T y, T z ) : w( w ), x( x ), y( y ), z( z ) {}
 		constexpr quat_( const quat_& o ) = default;
@@ -21,6 +25,13 @@ namespace xo
 
 		/// member access
 		T w, x, y, z;
+
+		/// container methods
+		size_t size() const { return 4; }
+		iterator begin() { return &w; }
+		iterator end() { return &w + 4; }
+		const_iterator begin() const { return &w; }
+		const_iterator end() const { return &w + 4; }
 
 		/// generate quat with zero rotation
 		static constexpr quat_<T> identity() { return quat_<T>( T(1), T(0), T(0), T(0) ); }
