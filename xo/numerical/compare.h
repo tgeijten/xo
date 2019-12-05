5cf3537604593c5ad82e9a6c0ba4e2d6b3ce3552
Thomas Geijtenbeek <tgeijten@gmail.com>
2019-12-05 15:41:23 +0100

    R: less/greater_or_equal() -> less/greater_than_equal() N: less_than_not_equal(), greater_than_not_equal()

--- xo/numerical/compare.h
+++ xo/numerical/compare.h
@@ -10,10 +10,18 @@ namespace xo
 	{ return abs( v1 - v2 ) <= abs( v1 ) * e; }
 
 	/// compare floating point variables using ample_epsilon
-	template< typename T > bool less_or_equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
+	template< typename T > bool less_than_or_equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
 	{ return v1 < v2 || equal( v1, v2, e ); }
 
 	/// compare floating point variables using ample_epsilon
-	template< typename T > bool greater_or_equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
+	template< typename T > bool less_than_not_equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
+	{ return v1 < v2 && !equal( v1, v2, e ); }
+
+	/// compare floating point variables using ample_epsilon
+	template< typename T > bool greater_than_or_equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
 	{ return v1 > v2 || equal( v1, v2, e ); }
+
+	/// compare floating point variables using ample_epsilon
+	template< typename T > bool greater_than_not_equal( T v1, T v2, T e = constants<T>::ample_epsilon() )
+	{ return v1 > v2 && !equal( v1, v2, e ); }
 }
