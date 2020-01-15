f7448f2e9fbc28664452036251e1db190f36805f
Thomas Geijtenbeek <tgeijten@gmail.com>
2020-01-15 16:18:35 +0100

    U: frange constructors, member renaming

--- xo/utility/frange.h
+++ xo/utility/frange.h
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <iterator>
+#include "xo/numerical/bounds.h"
 
 namespace xo
 {
@@ -11,29 +12,32 @@ namespace xo
 	struct frange
 	{
 		using value_type = T;
-		frange( T b, T e, T s ) : begin_( b ), end_( e ), steps_( std::round( ( e - b ) / s ) ) {}
-		frange( const frange< T >& r, T s = T( 1 ) ) : begin_( r.begin_ ), end_( r.end_ ), steps_( s ) {}
-
+		frange( T lower, T upper, T increment ) : min_( lower ), max_( upper ), max_step_( std::round( ( upper - lower ) / increment ) ) {}
+		frange( T lower, T upper, int steps ) : min_( lower ), max_( upper ), max_step_( steps - 1 ) {}
+		frange( bounds<T> bounds, T increment ) : min_( bounds.lower ), max_( bounds.upper ), max_step_( std::round( bounds.range() / increment ) ) {}
+		frange( bounds<T> bounds, int steps ) : min_( bounds.lower ), max_( bounds.upper ), max_step_( steps - 1 ) {}
+		
 		struct iterator
 		{
 			using iterator_category = std::forward_iterator_tag;
 			using value_type = T;
-			using difference_type = T;
+			using difference_type = size_t;
 			using pointer = T*;
 			using reference = T&;
 
-			iterator( T s, T b, T e, T steps ) : s_( s ), b_( b ), e_( e ), steps_( steps ) {}
-			T s_, b_, e_, steps_;
-			T operator++() { return s_ += T( 1 ); }
-			T operator++( int ) { s_ += T( 1 ); return steps_ - T( 1 ); }
-			bool operator==( const iterator& other ) { return other.s_ == s_; }
-			bool operator!=( const iterator& other ) { return other.s_ != s_; }
-			T operator*() { return b_ + s_ * ( e_ - b_ ) / steps_; }
+			iterator( T step, T b, T e, T steps ) : step_( step ), min_( b ), max_( e ), max_step_( steps ) {}
+			T step_, min_, max_, max_step_;
+			T operator++() { return step_ += T( 1 ); }
+			T operator++( int ) { step_ += T( 1 ); return max_step_ - T( 1 ); }
+			bool operator==( const iterator& other ) { return other.step_ == step_; }
+			bool operator!=( const iterator& other ) { return other.step_ != step_; }
+			T operator*() { return min_ + step_ * ( max_ - min_ ) / max_step_; }
 		};
 
-		iterator begin() const { return iterator( T( 0 ), begin_, end_, steps_ ); }
-		iterator end() const { return iterator( steps_ + 1, begin_, end_, steps_ ); }
+		iterator begin() const { return iterator( T( 0 ), min_, max_, max_step_ ); }
+		iterator end() const { return iterator( max_step_ + 1, min_, max_, max_step_ ); }
+		size_t size() const { return size_t( max_step_ ); }
 
-		T begin_, end_, steps_;
+		T min_, max_, max_step_;
 	};
 }
