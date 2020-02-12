67ac9d83ddbb7026e9dadcc4043354e5c316b2dd
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2020-02-12 14:49:42 +0100

    U: file_resource_cache now uses std::map for iterator validity, improved code readability

--- xo/utility/file_resource_cache.h
+++ xo/utility/file_resource_cache.h
@@ -1,14 +1,13 @@
 #pragma once
 
-#include "xo/container/flat_map.h"
 #include "xo/filesystem/filesystem.h"
 #include "xo/utility/pointer_types.h"
 #include "xo/container/container_tools.h"
 #include "xo/string/string_type.h"
 
-#include <functional>
 #include <mutex>
 #include <ctime>
+#include <map>
 
 namespace xo
 {
@@ -19,26 +18,25 @@ namespace xo
 		{
 			std::lock_guard< std::mutex > lock( mutex_ );
 
-			// make sure file exists
-			xo_error_if( !file_exists( p ), "File does not exist: " + to_str( p ) );
+			auto it = cached_items_.find( p );
+			cached_item* item = it != cached_items_.end() ? &it->second : nullptr;
 
-			// find file in cache
-			auto it = cache_.find( p );
-			if ( it != cache_.end() )
+			if ( item )
 			{
+				// check if file was modified
 				auto wall_time = std::time( nullptr );
-				if ( std::difftime( wall_time, it->second.last_checked_time_ ) > min_check_modified_interval )
+				if ( std::difftime( wall_time, item->last_checked_time_ ) > min_check_modified_interval )
 				{
 					try
 					{
 						// check if the file has been modified
-						it->second.last_checked_time_ = wall_time;
+						item->last_checked_time_ = wall_time;
 						auto file_time = last_write_time( p );
-						if ( file_time != it->second.file_time_ )
+						if ( file_time != item->file_time_ )
 						{
 							// file was modified, reload
-							it->second.file_time_ = file_time;
-							it->second.data_ = std::make_unique<T>( p );
+							item->file_time_ = file_time;
+							item->data_ = std::make_unique<T>( p );
 						}
 					}
 					catch( std::exception& e ) {
@@ -48,25 +46,30 @@ namespace xo
 			}
 			else
 			{				
+				// file not found in cache, create new cached_item
+				xo_error_if( !file_exists( p ), "File does not exist: " + to_str( p ) );
 				try
 				{
-					// file not found in cache, create new item and put it in the cache
-					it = cache_.insert( std::make_pair( p, cached_item() ) ).first;
-					it->second.file_time_ = last_write_time( p );
-					it->second.last_checked_time_ = std::time( nullptr );
-					it->second.data_ = std::make_unique<T>( p );
+					auto [it, is_new_item] = cached_items_.insert( std::make_pair( p, cached_item() ) );
+					item = &it->second;
+					if ( is_new_item )
+					{
+						item->file_time_ = last_write_time( p );
+						item->last_checked_time_ = std::time( nullptr );
+						item->data_ = std::make_unique<T>( p );
+					}
 				}
 				catch ( std::exception& e ) {
 					xo_error( "Error creating resource: "s + e.what() );
 				}
 			}
 
-			// at this point, 'it' is a valid iterator, so we can return a copy
-			return std::unique_ptr< T >( new T( *it->second.data_ ) );
+			// at this point, item is valid and we can return a copy
+			return std::make_unique<T>( *item->data_ );
 		}
 
 	private:
-		static constexpr double min_check_modified_interval = 1.0; // interval in seconds
+		static constexpr double min_check_modified_interval = 3.0; // interval in seconds
 
 		std::mutex mutex_;
 		struct cached_item {
@@ -74,6 +77,6 @@ namespace xo
 			time_t last_checked_time_;
 			u_ptr<T> data_;
 		};
-		flat_map< PathT, cached_item > cache_;
+		std::map< PathT, cached_item > cached_items_;
 	};
 }
