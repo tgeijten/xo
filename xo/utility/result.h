149a95c0903217395998fde5dd8531865a44ec48
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2020-03-06 14:01:51 +0100

    N*: result.h, for results with optional error messages

---
+++ xo/utility/result.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "xo/string/string_type.h"
+
+namespace xo
+{
+	struct error_message {
+		error_message() : message_() {}
+		error_message( const char* msg ) : message_( msg ) {};
+		error_message( const string& msg ) : message_( msg ) {};
+		error_message( string&& msg ) : message_( std::move( msg ) ) {};
+
+		explicit operator bool() const { return message_.empty(); }
+		const string message() const { return message_; }
+
+	private:
+		string message_;
+	};
+
+	template< typename T, typename E = error_message >
+	struct result {
+		result() = default;
+		result( const T& v ) : value_( v ), error_() {}
+		result( T&& v ) : value_( std::move( v ) ), error_() {}
+		result( const E& e ) : value_(), error_( e ) {}
+		result( E&& e ) : value_(), error_( std::move( e ) ) {}
+
+		explicit operator bool() { return static_cast<bool>( error_ ); }
+		const T& value() const { return value_; }
+		const E& error() const { return error_; }
+
+	public:
+		T value_;
+		E error_;
+	};
+}
