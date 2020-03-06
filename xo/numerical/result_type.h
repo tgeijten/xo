b0455fa78349fa7cf392f78880b1d9f1cdd7706e
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2020-03-06 12:41:02 +0100

    R: deprecated result_type.h

--- xo/numerical/result_type.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define xo_result_type( A_, B_ ) decltype( A_(1) + B(1) )
-
-namespace xo
-{
-	/// result types
-	template< typename A, typename B > struct result_type { typedef decltype( A(1) + B(1) ) type; };
-}
