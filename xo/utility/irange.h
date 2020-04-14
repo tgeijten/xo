f3a850c6345be2e2b162ecc6e2606ed0e87feeb4
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2020-04-14 20:23:47 +0200

    F: irange and irange_step no longer derive from deprecated std::iterator

--- xo/utility/irange.h
+++ xo/utility/irange.h
@@ -18,11 +18,15 @@ namespace xo
 		irange() : begin_( T( 0 ) ), end_( T( 0 ) ) {}
 		irange( T b, T e ) : begin_( b ), end_( e ) {}
 
-		struct iterator : public std::iterator< std::forward_iterator_tag, T, T >
+		struct iterator
 		{
+			using iterator_category = std::forward_iterator_tag;
 			using value_type = T;
+			using difference_type = size_t;
+			using pointer = T*;
+			using reference = T&;
+
 			iterator( T v ) : value_( v ) { xo_assert_msg( is_whole( v ), "irange requires whole numbers" ); }
-			T value_;
 			T operator++() { return ++value_; }
 			T operator++( int ) { return value_++; }
 			iterator operator+( const T& offset ) { return iterator( value_ + offset ); }
@@ -31,6 +35,8 @@ namespace xo
 			bool operator!=( const iterator& other ) { return other.value_ != value_; }
 			T operator-( const iterator& other ) { return value_ - other.value_; }
 			T operator*() { return value_; }
+
+			T value_;
 		};
 
 		iterator begin() const { return iterator( begin_ ); }
@@ -46,17 +52,24 @@ namespace xo
 		irange_step( T b, T e, T s = T( 1 ) ) : begin_( b ), end_( e ), step_( s ) {}
 		irange_step( const irange< T >& r, T s = T( 1 ) ) : begin_( r.begin_ ), end_( r.end_ ), step_( s ) {}
 
-		struct iterator : public std::iterator< std::forward_iterator_tag, T, T >
+		struct iterator
 		{
+			using iterator_category = std::forward_iterator_tag;
 			using value_type = T;
+			using difference_type = size_t;
+			using pointer = T*;
+			using reference = T&;
+
 			iterator( T v, T s ) : value_( v ), step_( s ) { xo_assert_msg( is_whole( v ) && is_whole( s ), "irange requires whole numbers" ); }
-			T value_, step_;
 			T operator-( const iterator& other ) { return ( value_ - other.value_ ) / step_; }
 			T operator++() { return value_ += step_; }
 			T operator++( int ) { value_ += step_; return value_ - step_; }
+
 			bool operator==( const iterator& other ) { return other.value_ == value_; }
 			bool operator!=( const iterator& other ) { return other.value_ != value_; }
 			T operator*() { return value_; }
+
+			T value_, step_;
 		};
 
 		iterator begin() const { return iterator( begin_, step_ ); }
