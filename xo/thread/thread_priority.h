ead2b540f3fd02273bcb4ceccda6e84387f434c5
Thomas Geijtenbeek <tgeijten@gmail.com>
2020-06-29 16:23:42 +0200

    F*: scoped_thread_priority now shows warning instead of calling unimplemented get_thread_priority()

--- xo/thread/thread_priority.h
+++ xo/thread/thread_priority.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "xo/system/xo_config.h"
+#ifndef XO_COMP_MSVC
+#	include "xo/system/log.h"
+#endif
 
 namespace xo
 {
@@ -20,9 +23,15 @@ namespace xo
 	XO_API void set_thread_priority( thread_priority_data p );
 	XO_API thread_priority_data get_thread_priority();
 
+#ifdef XO_COMP_MSVC
 	struct XO_API scoped_thread_priority {
 		scoped_thread_priority( thread_priority p ) : tpimpl_( get_thread_priority() ) { set_thread_priority( p ); }
 		~scoped_thread_priority() { set_thread_priority( tpimpl_ ); }
 		thread_priority_data tpimpl_;
 	};
+#else
+	struct scoped_thread_priority {
+		scoped_thread_priority( thread_priority p ) { log::warning( "get_thread_priority() is not implemented for this platform" ); }
+};
+#endif
 }
