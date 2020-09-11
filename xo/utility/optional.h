7c149ca82aa1346b0d6992f764b9ced1023ad7dc
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2020-09-11 10:39:47 +0200

    U: added non-const access to optional

--- xo/utility/optional.h
+++ xo/utility/optional.h
@@ -9,7 +9,7 @@ namespace xo
 {
 	template< typename T, typename E = void > struct optional;
 
-	/// optional value for integrals (but not bool) that uses a sentinel to represent no value
+	/// optional value for integrals (not bool!) that uses a sentinel to represent no value
 	template< typename T > struct optional< T, typename std::enable_if_t< std::is_integral_v<T> && !std::is_same_v<T, bool> > >
 	{
 		using value_type = T;
@@ -18,14 +18,16 @@ namespace xo
 		optional& operator=( const T& v ) { value_ = v; return *this; }
 		explicit operator bool() const { return value_ != constants<T>::sentinel(); }
 		const T& operator*() const { return value_; }
+		T& operator*() { return value_; }
 		const T* operator->() const { return &value_; }
+		T* operator->() { return &value_; }
 		void reset() { value_ = constants<T>::sentinel(); }
 		const T& value() const { xo_error_if( !*this, "xo::optional has no value" ); return value_; }
 	private:
 		T value_;
 	};
 
-	/// optional value for floating points that uses NaN to represent no value
+	/// optional value for floating point numbers that uses NaN to represent no value
 	template< typename T > struct optional< T, typename std::enable_if_t< std::is_floating_point_v<T> > >
 	{
 		using value_type = T;
@@ -34,7 +36,9 @@ namespace xo
 		optional& operator=( const T& v ) { value_ = v; return *this; }
 		explicit operator bool() const { return value_ == value_; }
 		const T& operator*() const { return value_; }
+		T& operator*() { return value_; }
 		const T* operator->() const { return &value_; }
+		T* operator->() { return &value_; }
 		void reset() { value_ = std::numeric_limits<T>::quiet_NaN(); }
 		const T& value() const { xo_error_if( !*this, "xo::optional has no value" ); return value_; }
 	private:
@@ -52,7 +56,9 @@ namespace xo
 		optional& operator=( T&& v ) { value_ = std::move( v ); has_value_ = true; return *this; }
 		explicit operator bool() const { return has_value_; }
 		const T& operator*() const { return value_; }
+		T& operator*() { return value_; }
 		const T* operator->() const { return &value_; }
+		T* operator->() { return &value_; }
 		void reset() { has_value_ = false; }
 		const T& value() const { xo_error_if( !*this, "xo::optional has no value" ); return value_; }
 	private:
