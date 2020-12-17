56a6356dcfc1accdc6530f16ece28c3bb4e7be7c
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2020-12-17 09:01:01 +0100

    U: input/output_iterator_stream now performs implicit conversion on types. This is useful for e.g. loading doubles from a stream of floats.

--- xo/container/stream_wrapper.h
+++ xo/container/stream_wrapper.h
@@ -11,7 +11,7 @@ namespace xo
 	{
 		using value_type = typename It::value_type;
 		input_iterator_stream( It pos ) : pos_( pos ) {}
-		input_iterator_stream& operator>>( value_type& v ) { v = *pos_++; return *this; }
+		template< typename T > input_iterator_stream& operator>>( T& v ) { v = T( *pos_++ ); return *this; }
 		It pos_;
 	};
 
@@ -22,7 +22,8 @@ namespace xo
 	{
 		using value_type = typename It::value_type;
 		output_iterator_stream( It pos ) : pos_( pos ) {}
-		output_iterator_stream& operator<<( const value_type& v ) { *pos_++ = v; return *this; }
+
+		template< typename T > output_iterator_stream& operator<<( const T& v ) { *pos_++ = static_cast<value_type>( v ); return *this; }
 		output_iterator_stream& operator<<( value_type&& v ) { *pos_++ = std::move( v ); return *this; }
 		It pos_;
 	};
@@ -33,28 +34,28 @@ namespace xo
 	{
 		using value_type = typename C::value_type;
 		push_back_stream( C& cont ) : cont_( cont ) {}
-		push_back_stream& operator<<( const value_type& v ) { cont_.push_back( v ); return *this; }
+		template< typename T > push_back_stream& operator<<( const T& v ) { cont_.push_back( static_cast<value_type>( v ) ); return *this; }
 		push_back_stream& operator<<( value_type&& v ) { cont_.push_back( std::move( v ) ); return *this; }
 		C& cont_;
 	};
 
 	/// support for vec3_
-	template< typename It >
-	output_iterator_stream<It>& operator<<( output_iterator_stream<It>& str, const vec3_<typename It::value_type>& v ) {
+	template< typename It, typename T >
+	output_iterator_stream<It>& operator<<( output_iterator_stream<It>& str, const vec3_<T>& v ) {
 		return str << v.x << v.y << v.z;
 	}
-	template< typename It >
-	input_iterator_stream<It>& operator>>( input_iterator_stream<It>& str, vec3_<typename It::value_type>& v ) {
+	template< typename It, typename T >
+	input_iterator_stream<It>& operator>>( input_iterator_stream<It>& str, vec3_<T>& v ) {
 		return str >> v.x >> v.y >> v.z;
 	}
 
 	/// support for quat_
-	template< typename It >
-	output_iterator_stream<It>& operator<<( output_iterator_stream<It>& str, const quat_<typename It::value_type>& v ) {
+	template< typename It, typename T >
+	output_iterator_stream<It>& operator<<( output_iterator_stream<It>& str, const quat_<T>& v ) {
 		return str << v.w << v.x << v.y << v.z;
 	}
-	template< typename It >
-	input_iterator_stream<It>& operator>>( input_iterator_stream<It>& str, quat_<typename It::value_type>& v ) {
+	template< typename It, typename T >
+	input_iterator_stream<It>& operator>>( input_iterator_stream<It>& str, quat_<T>& v ) {
 		return str >> v.w >> v.x >> v.y >> v.z;
 	}
 }
