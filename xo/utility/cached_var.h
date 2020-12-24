a854396f169f35be55763081028e2514b41fa0a3
Thomas Geijtenbeek <tgeijten@gmail.com>
2020-12-24 17:07:30 +0100

    F: cached_var now requires initialization, since default 0 for TimeT is not a good idea

--- xo/utility/cached_var.h
+++ xo/utility/cached_var.h
@@ -5,7 +5,7 @@ namespace xo
 	template< typename ValueT, typename TimeT >
 	struct cached_var
 	{
-		cached_var( ValueT init_val = ValueT(), TimeT init_time = TimeT() ) : value_( init_val ), time_( init_time ) {}
+		cached_var( ValueT init_val, TimeT init_time ) : value_( init_val ), time_( init_time ) {}
 
 		template< typename Fun >
 		const ValueT& operator()( const TimeT& t, const Fun& f ) {
