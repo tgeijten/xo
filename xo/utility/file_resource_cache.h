c97ddeb5381d0f131ccdadfb9e5a0babcfb9f57f
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2021-02-22 14:50:46 +0100

    U!: file_resource_cache now uses a read-write lock for better performance

--- xo/utility/file_resource_cache.h
+++ xo/utility/file_resource_cache.h
@@ -6,6 +6,7 @@
 #include "xo/string/string_type.h"
 
 #include <mutex>
+#include <shared_mutex>
 #include <ctime>
 #include <map>
 
@@ -16,19 +17,19 @@ namespace xo
 	{
 		std::unique_ptr< T > operator()( const PathT& p )
 		{
-			std::lock_guard< std::mutex > lock( mutex_ );
+			std::shared_lock read_lock( mutex_ );
 
-			auto it = cached_items_.find( p );
-			cached_item* item = it != cached_items_.end() ? &it->second : nullptr;
-
+			cached_item* item = find( p );
 			if ( item )
 			{
 				// check if file was modified
 				auto wall_time = std::time( nullptr );
 				if ( std::difftime( wall_time, item->last_checked_time_ ) > min_check_modified_interval )
 				{
+					read_lock.unlock();
 					try
 					{
+						std::unique_lock write_lock( mutex_ );
 						// check if the file has been modified
 						item->last_checked_time_ = wall_time;
 						auto file_time = last_write_time( p );
@@ -42,39 +43,57 @@ namespace xo
 					catch( std::exception& e ) {
 						xo_error( "Error updating resource: "s + e.what() );
 					}
+					read_lock.lock();
 				}
 			}
 			else
 			{				
 				// file not found in cache, create new cached_item
 				xo_error_if( !file_exists( p ), "File does not exist: " + to_str( p ) );
+
+				read_lock.unlock();
 				try
 				{
-					cached_item new_item;
-					new_item.file_time_ = last_write_time( p );
-					new_item.last_checked_time_ = std::time( nullptr );
-					new_item.data_ = std::make_unique<T>( p );
-					auto [it, is_new_item] = cached_items_.insert( std::make_pair( p, std::move( new_item ) ) );
-					item = &it->second;
+					std::unique_lock write_lock( mutex_ );
+					// find the item again, to make sure another thread didn't just create it
+					item = find( p );
+					if ( !item ) 
+					{
+						cached_item new_item;
+						new_item.file_time_ = last_write_time( p );
+						new_item.last_checked_time_ = std::time( nullptr );
+						new_item.data_ = std::make_unique<T>( p );
+						auto [it, is_new_item] = cached_items_.insert( std::make_pair( p, std::move( new_item ) ) );
+						item = &it->second;
+					}
 				}
 				catch ( std::exception& e ) {
 					xo_error( "Error creating resource: "s + e.what() );
 				}
+				read_lock.lock();
 			}
 
-			// at this point, item is valid and we can return a copy
+			// at this point, read_load holds the mutex and item is valid
 			return std::make_unique<T>( *item->data_ );
 		}
 
 	private:
 		static constexpr double min_check_modified_interval = 3.0; // interval in seconds
 
-		std::mutex mutex_;
 		struct cached_item {
 			time_t file_time_;
 			time_t last_checked_time_;
 			u_ptr<T> data_;
 		};
+
+		std::shared_mutex mutex_;
 		std::map< PathT, cached_item > cached_items_;
+
+		cached_item* find( const PathT& p ) {
+			auto it = cached_items_.find( p );
+			if ( it != cached_items_.end() )
+				return &it->second;
+			else return nullptr;
+		}
 	};
 }
