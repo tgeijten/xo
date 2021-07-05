163bf2ae2026cadfca089702812c28e77925b321
Thomas Geijtenbeek <thomas@goatstream.com>
2021-07-05 16:53:56 +0200

    U: rotation_vector/axis_angle_from_quat now checks if q.w < 1 instead of throwing

--- xo/geometry/quat.h
+++ xo/geometry/quat.h
@@ -141,8 +141,7 @@ namespace xo
 	/// Get rotation vector from normalized quaternion
 	template< typename T > vec3_<T> rotation_vector_from_quat( const quat_<T>& q ) {
 		T l = sqrt( q.x * q.x + q.y * q.y + q.z * q.z );
-		if ( l > constants<T>::ample_epsilon() ) {
-			xo_debug_assert( q.w <= T( 1 ) );
+		if ( ( l > constants<T>::ample_epsilon() ) & ( q.w < T( 1 ) ) ) {
 			T f = T( 2 ) * std::acos( q.w ) / l;
 			return vec3_<T>( f * q.x, f * q.y, f * q.z );
 		} else return vec3_<T>::zero();
@@ -151,8 +150,7 @@ namespace xo
 	/// Get axis angle from normalized quaternion
 	template< typename T > std::pair< vec3_<T>, radian_<T> > axis_angle_from_quat( const quat_<T>& q ) {
 		T l = sqrt( q.x * q.x + q.y * q.y + q.z * q.z );
-		if ( l > constants<T>::ample_epsilon() ) {
-			xo_debug_assert( q.w <= T( 1 ) );
+		if ( ( l > constants<T>::ample_epsilon() ) & ( q.w < T( 1 ) ) ) {
 			T s = T( 1 ) / l;
 			return { vec3f( s * q.x, s * q.y, s * q.z ), radian_<T>( T( 2 ) * std::acos( q.w ) ) };
 		} else return { vec3_<T>::unit_x(), radian_<T>() };
