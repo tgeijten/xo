065e2fef4afc5d021184f7084957e724d5d8e683
Thomas Geijtenbeek <thomas@goatstream.com>
2021-09-02 11:43:46 +0200

    U!: file_resource_cache now supports custom create() and clone() functions

--- xo/utility/file_resource_cache.h
+++ xo/utility/file_resource_cache.h
@@ -15,33 +15,40 @@ namespace xo
 	template< typename T, typename PathT >
 	struct file_resource_cache
 	{
-		std::unique_ptr< T > operator()( const PathT& p )
+		using create_function = std::function<T* ( const PathT& )>;
+		using clone_function = std::function<T* ( const T& )>;
+
+		file_resource_cache( create_function cr = default_create, clone_function cl = default_clone ) :
+			create_( std::move( cr ) ), clone_( std::move( cl ) ) {}
+
+		std::unique_ptr<T> operator()( const PathT& p )
 		{
 			std::shared_lock read_lock( mutex_ );
 
 			cached_item* item = find( p );
 			if ( item )
 			{
-				// check if file was modified
+				// check how long ago we checked if the file was modified
 				auto wall_time = std::time( nullptr );
 				if ( std::difftime( wall_time, item->last_checked_time_ ) > min_check_modified_interval )
 				{
+					// we haven't checked for a while
 					read_lock.unlock();
 					try
 					{
-						std::unique_lock write_lock( mutex_ );
 						// check if the file has been modified
+						std::unique_lock write_lock( mutex_ );
 						item->last_checked_time_ = wall_time;
 						auto file_time = last_write_time( p );
 						if ( file_time != item->file_time_ )
 						{
 							// file was modified, reload
 							item->file_time_ = file_time;
-							item->data_ = std::make_unique<T>( p );
+							item->data_.reset( create_( p ) );
 						}
 					}
 					catch( std::exception& e ) {
-						xo_error( "Error updating resource: "s + e.what() );
+						xo_error( "Error reading " + to_str( p ) + ": " + e.what() );
 					}
 					read_lock.lock();
 				}
@@ -62,22 +69,25 @@ namespace xo
 						cached_item new_item;
 						new_item.file_time_ = last_write_time( p );
 						new_item.last_checked_time_ = std::time( nullptr );
-						new_item.data_ = std::make_unique<T>( p );
+						new_item.data_.reset( create_( p ) );
 						auto [it, is_new_item] = cached_items_.insert( std::make_pair( p, std::move( new_item ) ) );
 						item = &it->second;
 					}
 				}
 				catch ( std::exception& e ) {
-					xo_error( "Error creating resource: "s + e.what() );
+					xo_error( "Error reading " + to_str( p ) + ": " + e.what() );
 				}
 				read_lock.lock();
 			}
 
 			// at this point, read_load holds the mutex and item is valid
-			return std::make_unique<T>( *item->data_ );
+			return std::unique_ptr<T>( clone_( *item->data_ ) );
 		}
 
 	private:
+		static T* default_create( const PathT& f ) { return new T( f ); }
+		static T* default_clone( const T& o ) { return new T( o ); }
+
 		static constexpr double min_check_modified_interval = 3.0; // interval in seconds
 
 		struct cached_item {
@@ -95,5 +105,8 @@ namespace xo
 				return &it->second;
 			else return nullptr;
 		}
+
+		create_function create_;
+		clone_function clone_;
 	};
 }
