55fb035023943072e4d129527afd9dab0f0e50e9
Thomas Geijtenbeek <tgeijten@gmail.com>
2021-09-27 16:20:50 +0200

    U!: memoize now returns reference to cached object as result

--- xo/utility/memoize.h
+++ xo/utility/memoize.h
@@ -12,13 +12,11 @@ namespace xo
 	{
 		memoize( std::function< R( Args... )> f  ) : func_( f ) {}
 
-		R operator()( Args... args ) {
+		const R& operator()( Args... args ) {
 			auto tup = std::make_tuple( args... );
 			auto it = mem_.find( tup );
 			if ( it == mem_.end() ) {
-				auto r = func_( args... );
-				mem_[ tup ] = r;
-				return r;
+				return mem_[ tup ] = func_( args... );
 			}
 			else
 				return it->second;
