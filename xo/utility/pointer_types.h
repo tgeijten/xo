7629c4e874ee6eba73f04ae447acbca06c759a70
Thomas Geijtenbeek <thomas@goatstream.com>
2021-10-15 15:16:31 +0200

    N: dynamic_unique_cast(), try_dynamic_unique_cast()

--- xo/utility/pointer_types.h
+++ xo/utility/pointer_types.h
@@ -6,4 +6,28 @@ namespace xo
 {
 	template< typename T > using u_ptr = std::unique_ptr< T >;
 	template< typename T > using s_ptr = std::shared_ptr< T >;
+
+	// Try to perform a dynamic_cast on a unique pointer
+	// SUCCESS: ownership is transfered to the returned pointer
+	// FAILURE: original pointer maintains ownership, returns nullptr
+	template <typename TargetT, typename SourceT>
+	u_ptr<TargetT> try_dynamic_unique_cast( u_ptr<SourceT>& ptr ) {
+		if ( TargetT* tptr = dynamic_cast<TargetT*>( ptr.get() ) ) {
+			ptr.release();
+			return u_ptr<TargetT>( tptr );
+		}
+		else return u_ptr<TargetT>();
+	}
+
+	// Perform a dynamic_cast on a unique pointer
+	// SUCCESS: ownership is transfered to the returned pointer
+	// FAILURE: original pointer is deleted, returns nullptr
+	template <typename TargetT, typename SourceT>
+	u_ptr<TargetT> dynamic_unique_cast( u_ptr<SourceT>&& ptr ) {
+		if ( TargetT* tptr = dynamic_cast<TargetT*>( ptr.get() ) ) {
+			ptr.release();
+			return u_ptr<TargetT>( tptr );
+		}
+		else return u_ptr<TargetT>();
+	}
 }
