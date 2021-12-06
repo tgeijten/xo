af83fd3c53d75578f013bcfe99f54f1c51b98c1d
Thomas Geijtenbeek <thomas@goatstream.com>
2021-12-06 17:23:03 +0100

    U: version comparison is now complete and robust, removed version::to_int100()

--- xo/system/version.h
+++ xo/system/version.h
@@ -8,10 +8,9 @@ namespace xo
 	struct version
 	{
 		version() : major_(), minor_(), patch_(), build_(), postfix_() {}
-		version( int maj, int min, int bugfix, int bld = 0, string post = "" ) :
-			major_( maj ), minor_( min ), patch_( bugfix ), build_( bld ), postfix_( post ) {}
+		version( int maj, int min, int patch, int bld = 0, string post = "" ) :
+			major_( maj ), minor_( min ), patch_( patch ), build_( bld ), postfix_( post ) {}
 
-		int to_int100() const { return major_ * 10000 + minor_ * 100 + patch_; }
 		bool empty() const { return major_ == 0 && minor_ == 0 && patch_ == 0 && build_ == 0 && postfix_.empty(); }
 
 		int major_;
@@ -21,7 +20,30 @@ namespace xo
 		string postfix_;
 	};
 
-	inline bool operator<( const version& v1, const version& v2 ) { return v1.to_int100() < v2.to_int100(); }
-	inline bool operator==( const version& v1, const version& v2 ) { return v1.to_int100() == v2.to_int100(); }
-	inline bool operator!=( const version& v1, const version& v2 ) { return v1.to_int100() != v2.to_int100(); }
+	inline int version_compare( const version& v1, const version& v2 ) {
+		if ( v1.major_ < v2.major_ )
+			return -1;
+		else if ( v1.major_ > v2.major_ )
+			return 1;
+		else if ( v1.minor_ < v2.minor_ )
+			return -2;
+		else if ( v1.minor_ > v2.minor_ )
+			return 2;
+		else if ( v1.patch_ < v2.patch_ )
+			return -3;
+		else if ( v1.patch_ > v2.patch_ )
+			return 3;
+		else if ( v1.build_ < v2.build_ )
+			return -4;
+		else if ( v1.build_ > v2.build_ )
+			return 4;
+		else return 0;
+	}
+
+	inline bool operator<( const version& v1, const version& v2 ) { return version_compare( v1, v2 ) < 0; }
+	inline bool operator>( const version& v1, const version& v2 ) { return version_compare( v1, v2 ) > 0; }
+	inline bool operator<=( const version& v1, const version& v2 ) { return version_compare( v1, v2 ) <= 0; }
+	inline bool operator>=( const version& v1, const version& v2 ) { return version_compare( v1, v2 ) >= 0; }
+	inline bool operator==( const version& v1, const version& v2 ) { return version_compare( v1, v2 ) == 0; }
+	inline bool operator!=( const version& v1, const version& v2 ) { return version_compare( v1, v2 ) != 0; }
 }
