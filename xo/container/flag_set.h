26580b61e561ac83059ee1068ec793bdfdbcbab6
Thomas Geijtenbeek <tgeijten@gmail.com>
2022-01-05 14:16:23 +0100

    N: flag_set::any( initializer_list ), flag_set::count( initializer_list )

--- xo/container/flag_set.h
+++ xo/container/flag_set.h
@@ -10,33 +10,75 @@ namespace xo
 	template< typename EnumT, typename StorageT = uint64 >
 	struct flag_set
 	{
-		flag_set() : data_( StorageT( 0 ) ) {}
+		flag_set() : data_() {}
 		flag_set( const flag_set& o ) = default;
-		flag_set( std::initializer_list< EnumT > flags ) : data_( StorageT( 0 ) ) { set( flags ); }
+		flag_set( std::initializer_list<EnumT> flags ) : data_( ) { set( flags ); }
 		flag_set( EnumT flag ) : data_( storage_bit( flag ) ) {}
 		explicit flag_set( StorageT data ) : data_( data ) {}
 
 		flag_set& operator=( const flag_set& o ) { data_ = o.data_; return *this; }
 
-		inline bool get( EnumT index ) const { return ( data_ & storage_bit( index ) ) != 0; }
-		template< EnumT index >	bool get() const { return ( data_ & storage_bit( index ) ) != 0; }
-		inline bool operator()( EnumT index ) const { return get( index ); }
-
-		flag_set& set( EnumT index, bool value ) { data_ = ( data_ & ~storage_bit( index ) ) | storage_bit( index, value ); return *this; }
-		template< EnumT index > flag_set& set( bool value ) { data_ = ( data_ & ~storage_bit( index ) ) | storage_bit( index, value ); return *this; }
-
-		flag_set& set( EnumT index ) { data_ |= storage_bit( index ); return *this; }
-		template< EnumT index > flag_set& set() { data_ |= storage_bit( index ); return *this; }
-		template< EnumT index > flag_set& clear() { data_ &= ~storage_bit( index ); return *this; }
-
-		flag_set& toggle( EnumT index ) { set( index, !get( index ) ); return *this; }
-		template< EnumT index > flag_set& toggle() { set<index>( !get<index>() ); return *this; }
-
-		flag_set& set( std::initializer_list< EnumT > flags ) { for ( auto f : flags ) set( f ); return *this; }
-		flag_set& set( std::initializer_list< EnumT > flags, bool value ) { for ( auto f : flags ) set( f, value ); return *this; }
-		flag_set& clear( std::initializer_list< EnumT > flags ) { for ( auto f : flags ) clear( f ); return *this; }
-
-		void clear() { data_ = StorageT(0); }
+		inline bool get( EnumT index ) const {
+			return ( data_ & storage_bit( index ) ) != 0;
+		}
+		template< EnumT index >	bool get() const {
+			return ( data_ & storage_bit( index ) ) != 0;
+		}
+		inline bool operator()( EnumT index ) const {
+			return get( index );
+		}
+		flag_set& set( EnumT index, bool value ) {
+			data_ = ( data_ & ~storage_bit( index ) ) | storage_bit( index, value );
+			return *this;
+		}
+		template< EnumT index > flag_set& set( bool value ) {
+			data_ = ( data_ & ~storage_bit( index ) ) | storage_bit( index, value );
+			return *this;
+		}
+		flag_set& set( EnumT index ) {
+			data_ |= storage_bit( index );
+			return *this;
+		}
+		template< EnumT index > flag_set& set() {
+			data_ |= storage_bit( index );
+			return *this;
+		}
+		template< EnumT index > flag_set& clear() {
+			data_ &= ~storage_bit( index );
+			return *this;
+		}
+		flag_set& toggle( EnumT index ) {
+			set( index, !get( index ) );
+			return *this;
+		}
+		template< EnumT index > flag_set& toggle() {
+			set<index>( !get<index>() );
+			return *this;
+		}
+		flag_set& set( std::initializer_list<EnumT> flags ) {
+			for ( auto f : flags ) set( f );
+			return *this;
+		}
+		flag_set& clear( std::initializer_list<EnumT> flags ) {
+			for ( auto f : flags ) clear( f );
+			return *this;
+		}
+		flag_set& set( std::initializer_list<EnumT> flags, bool value ) {
+			for ( auto f : flags ) set( f, value );
+			return *this;
+		}
+		bool any( std::initializer_list<EnumT> flags ) {
+			for ( auto f : flags )
+				if get( f ) return true;
+			return false;
+		}
+		size_t count( std::initializer_list<EnumT> flags ) {
+			size_t c = 0;
+			for ( auto f : flags )
+				c += static_cast<size_t>( get( f ) );
+			return c;
+		}
+		void clear() { data_ = StorageT( 0 ); }
 		bool any() const { return data_ != 0; }
 		StorageT data() const { return data_; }
 
