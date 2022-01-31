970324efe616e3ee0a23d0bc2124af71de91f5ce
Thomas Geijtenbeek <tgeijten@gmail.com>
2022-01-31 16:54:47 +0100

    N: index_test.cpp

---
+++ xo_test/index_test.cpp
@@ -0,0 +1,16 @@
+#include "xo/system/test_case.h"
+#include "xo/utility/index.h"
+
+namespace xo
+{
+	XO_TEST_CASE( xo_index_test )
+	{
+		std::vector<double> v{ 1.0, 2.0, 3.0, 4.0 };
+		auto idx = xo::index_class<double>( 1 );
+
+		XO_CHECK( v[ idx.value() ] == 2.0 );
+		XO_CHECK( v[ ( idx + 1 ).value() ] == 3.0 );
+		auto i3 = idx + 2;
+		XO_CHECK( v[ i3.value() ] == 4.0 );
+	}
+}
