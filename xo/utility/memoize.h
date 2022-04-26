7efe9bfa227036718a76ff3b3c5230a2c8e54f4e
Thomas Geijtenbeek <thomas@goatstream.com>
2022-04-26 16:58:53 +0200

    N: memoize_thread_safe

--- xo/utility/memoize.h
+++ xo/utility/memoize.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <functional>
+#include <shared_mutex>
 
 namespace xo
 {
@@ -22,8 +23,47 @@ namespace xo
 				return it->second;
 		}
 		
+		size_t size() const { return mem_.size(); }
+
 	private:
 		std::map< std::tuple< Args... >, R > mem_;
 		std::function< R( Args... ) > func_;
+	};
+
+	template <typename T>
+	struct memoize_thread_safe;
+
+	template< typename R, typename... Args >
+	struct memoize_thread_safe< R( Args... ) >
+	{
+		memoize_thread_safe( std::function< R( Args... )> f  ) : func_( f ) {}
+
+		const R& operator()( Args... args ) {
+			auto tup = std::make_tuple( args... );
+			std::shared_lock read_lock( mutex_ );
+			auto it = mem_.find( tup );
+			if ( it == mem_.end() ) {
+				read_lock.unlock();
+				std::unique_lock write_lock( mutex_ );
+				try {
+					if ( it = mem_.find( tup ); it != mem_.end() )
+						return it->second; // another thread got a unique_lock first
+					else 
+						return mem_[ tup ] = func_( args... );
+				}
+				catch ( std::exception& e ) {
+					xo_error( e.what() );
+				}
+			}
+			else
+				return it->second;
+		}
+
+		size_t size() { std::shared_lock read_lock( mutex_ ); return mem_.size(); }
+		
+	private:
+		std::map< std::tuple< Args... >, R > mem_;
+		std::function< R( Args... ) > func_;
+		std::shared_mutex mutex_;
 	};
 }
