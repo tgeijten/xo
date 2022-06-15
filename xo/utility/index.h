5a209fce7038bb988637d0abf2525190801b8d16
Thomas Geijtenbeek <thomas@goatstream.com>
2022-06-15 23:20:12 +0200

    R: index::value_ -> index::idx

--- xo/utility/index.h
+++ xo/utility/index.h
@@ -12,41 +12,41 @@ namespace xo
 		using index_type = I;
 
 		// default, copy and move constructor
-		constexpr index_class() : value_( invalid() ) {}
+		constexpr index_class() : idx( invalid_index_value() ) {}
 		index_class( const index_class& o ) = default;
-		index_class( index_class&& o ) noexcept : value_( o.value_ ) { o.reset(); }
+		index_class( index_class&& o ) noexcept : idx( o.idx ) { o.reset(); }
 
 		// explicit conversion constructors
-		explicit constexpr index_class( index_type v ) : value_( v ) {}
-		template<typename U> explicit constexpr index_class( index_class<U, I> o ) : value_( o.value() ) {}
+		explicit constexpr index_class( index_type v ) : idx( v ) {}
+		template<typename U> explicit constexpr index_class( index_class<U, I> o ) : idx( o.value() ) {}
 
 		// default copy and move assignment
-		index_class& operator=( const index_class& o ) { value_ = o.value_; return *this; }
-		index_class& operator=( index_class&& o ) noexcept { std::swap( value_, o.value_ ); return *this; }
+		index_class& operator=( const index_class& o ) { idx = o.idx; return *this; }
+		index_class& operator=( index_class&& o ) noexcept { std::swap( idx, o.idx ); return *this; }
 
-		explicit operator bool() const { return value_ != invalid(); }
+		explicit operator bool() const { return idx != invalid_index_value(); }
 
-		void reset() { value_ = invalid(); }
+		void reset() { idx = invalid_index_value(); }
 
-		template< typename C > auto iter( const C& c ) const { return c.begin() + value_; }
-		template< typename C > auto iter( C& c ) const { return c.begin() + value_; }
+		template< typename C > auto iter( const C& c ) const { return c.begin() + idx; }
+		template< typename C > auto iter( C& c ) const { return c.begin() + idx; }
 
 		template< typename C > static index_class end( const C& c ) { return index_class( index_type( c.size() ) ); }
 		template< typename C > static index_class back( const C& c ) { return index_class( index_type( c.size() ) - 1 ); }
 
-		index_class operator+( index_type ofs ) const { return index_class( value_ + ofs ); }
-		index_class operator-( index_type ofs ) const { return index_class( value_ - ofs ); }
+		index_class operator+( index_type ofs ) const { return index_class( idx + ofs ); }
+		index_class operator-( index_type ofs ) const { return index_class( idx - ofs ); }
 
-		index_class& operator++() { ++value_; return *this; }
-		index_class operator++( int ) { index_class org( value_ ); ++value_; return org; }
+		index_class& operator++() { ++idx; return *this; }
+		index_class operator++( int ) { index_class org( idx ); ++idx; return org; }
 
-		friend bool operator<( const index_class a, const index_class b ) { return a.value_ < b.value_; }
-		friend bool operator==( const index_class a, const index_class b ) { return a.value_ == b.value_; }
-		friend bool operator!=( const index_class a, const index_class b ) { return a.value_ != b.value_; }
+		friend bool operator<( const index_class a, const index_class b ) { return a.idx < b.idx; }
+		friend bool operator==( const index_class a, const index_class b ) { return a.idx == b.idx; }
+		friend bool operator!=( const index_class a, const index_class b ) { return a.idx != b.idx; }
 
-		index_type value_;
+		index_type idx;
 
 	protected:
-		static constexpr index_type invalid() { return ~index_type( 0 ); }
+		static constexpr index_type invalid_index_value() { return ~index_type( 0 ); }
 	};
 }
--- xo_test/index_test.cpp
+++ xo_test/index_test.cpp
@@ -8,9 +8,9 @@ namespace xo
 		std::vector<double> v{ 1.0, 2.0, 3.0, 4.0 };
 		auto idx = xo::index_class<double>( 1 );
 
-		XO_CHECK( v[ idx.value_ ] == 2.0 );
-		XO_CHECK( v[ ( idx + 1 ).value_ ] == 3.0 );
+		XO_CHECK( v[ idx.idx ] == 2.0 );
+		XO_CHECK( v[ ( idx + 1 ).idx ] == 3.0 );
 		auto i3 = idx + 2;
-		XO_CHECK( v[ i3.value_ ] == 4.0 );
+		XO_CHECK( v[ i3.idx ] == 4.0 );
 	}
 }
