6f948943efbe375d0045d8a2c685a1a2a2fa9ade
Thomas Geijtenbeek <thomas@goatstream.com>
2024-05-22 11:15:24 +0200

    N: minimum_jerk function

--- xo/numerical/functions.h
+++ xo/numerical/functions.h
@@ -6,8 +6,7 @@ namespace xo
 {
 	// fast implementation of arccos, approx 4x faster than standard math.h
 	// https://stackoverflow.com/questions/3380628/fast-arc-cos-algorithm/3380723#3380723
-	template< typename T > T fast_acos( T x )
-	{
+	template< typename T > T fast_acos( T x ) {
 		constexpr T a = T( -0.939115566365855 );
 		constexpr T b = T( 0.9217841528914573 );
 		constexpr T c = T( -1.2845906244690837 );
@@ -19,5 +18,13 @@ namespace xo
 		num += b * x3;
 		den += d * x3 * x;
 		return constants<T>::half_pi() + num / den;
+	}
+
+	// minimum jerk curve for x in [0, 1]
+	template< typename T > T minimum_jerk( T x ) {
+		auto x3 = x * x * x;
+		auto x4 = x3 * x;
+		auto x5 = x4 * x;
+		return 6 * x5 - 15 * x4 + 10 * x3;
 	}
 }
