c88a28331dc28277c316b47cd8ce45e9c6f29c8e
Thomas Geijtenbeek <thomas@goatstream.com>
2025-09-01 19:32:48 +0200

    N: log::make_level(int)

--- xo/system/log_level.h
+++ xo/system/log_level.h
@@ -22,5 +22,10 @@ namespace xo
 		inline constexpr const char* level_names_full[] = {
 			"", "trace", "debug", "info", "warning", "error", "critical", ""
 		};
+
+		inline level make_level( int l ) {
+			if ( l < 0 ) l = 0; else if ( l > 7 ) l = 7;
+			return level( l );
+		}
 	}
 }
