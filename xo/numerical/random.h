d86eddbb87dfda0cc40d0352ddc2d32181b80596
Thomas Geijtenbeek <thomas@goatstream.com>
2025-11-07 10:56:45 +0100

    U: random_number_generator uniform_vec3 and uniform_quad updates

--- xo/numerical/random.h
+++ xo/numerical/random.h
@@ -17,18 +17,32 @@ namespace xo
 		void seed( unsigned int s ) { re.seed( s ); }
 
 		template< typename T > T uniform( T min, T max ) { return std::uniform_real_distribution<T>( min, max )( re ); }
-		template< typename T > T uniform( const xo::bounds<T>& b ) { return uniform<T>( b.lower, b.upper ); }
+		template< angle_unit U, typename T > angle_<U, T> uniform( angle_<U, T> min, angle_<U, T> max ) { return angle_<U, T>( uniform( min.value, max.value ) ); }
+		template< typename T > T uniform( const bounds<T>& b ) { return uniform( b.lower, b.upper ); }
 
 		template< typename T > T uniform_int( T min, T max ) { return std::uniform_int_distribution<T>( min, max )( re ); }
-		template< typename T > T uniform_int( const xo::bounds<T>& b ) { return uniform_int<T>( b.lower, b.upper ); }
+		template< typename T > T uniform_int( const bounds<T>& b ) { return uniform_int( b.lower, b.upper ); }
 
 		template< typename T > T normal( T mean, T stdev ) { return std::normal_distribution<T>( mean, stdev )( re ); }
 
 		template< typename T > vec3_<T> uniform_vec3( T min, T max ) {
 			std::uniform_real_distribution<T> rd{ min, max }; return { rd( re ), rd( re ), rd( re ) };
 		}
+		template< typename T > vec3_<T> uniform_vec3( const bounds<T>& b ) {
+			return uniform_vec3( b.lower, b.upper );
+		}
+		template< typename T > vec3_<T> uniform_vec3( const vec3_<bounds<T>>& b ) {
+			return { uniform( b.x ), uniform( b.y ), uniform( b.z ) };
+		}
+
 		template< typename T > quat_<T> uniform_quat( degree_<T> min, degree_<T> max ) {
-			auto ea = vec3deg_<T>( uniform_vec3<T>( min.value, max.value ) ); return quat_from_euler_xyz( ea );
+			return quat_from_euler_xyz( vec3deg_<T>( uniform_vec3( min.value, max.value ) ) );
+		}
+		template< typename T > quat_<T> uniform_quat( const bounds<degree_<T>>& b ) {
+			return uniform_quat( b.lower, b.upper );
+		}
+		template< typename T > quat_<T> uniform_quat( const vec3_<bounds<degree_<T>>>& b ) {
+			return quat_from_euler_xyz( uniform_vec3( b ) );
 		}
 
 	private:
