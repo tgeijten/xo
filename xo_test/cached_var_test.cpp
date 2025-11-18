f21e80cef0b604503508b987a5f668ebfaab906d
Thomas Geijtenbeek <thomas@goatstream.com>
2025-11-18 13:28:05 +0100

    U: cached_var_test

--- xo_test/cached_var_test.cpp
+++ xo_test/cached_var_test.cpp
@@ -6,10 +6,12 @@ namespace xo
 	XO_TEST_CASE( xo_cached_var_test )
 	{
 		cached_var<string, double> test( 0 );
+		int update = 0;
 
-		XO_CHECK( test( 0.0, []() { return "No update"; } ) == "" );
-		XO_CHECK( test( 1.0, []() { return "Test1"; } ) == "Test1" );
-		XO_CHECK( test( 1.0, []() { return "No update"; } ) == "Test1" );
-		XO_CHECK( test( 2.0, []() { return "Test2"; } ) == "Test2" );
+		XO_CHECK( test( 0.0, [&]() { ++update; return "No update"; } ) == "" );
+		XO_CHECK( test( 1.0, [&]() { ++update; return "Test1"; } ) == "Test1" );
+		XO_CHECK( test( 1.0, [&]() { ++update; return "No update"; } ) == "Test1" );
+		XO_CHECK( test( 2.0, [&]() { ++update; return "Test2"; } ) == "Test2" );
+		XO_CHECK( update == 2 );
 	}
 }
