043830fc5585b48aef0b996416ee4f1e005f7673
Thomas Geijtenbeek <thomas@goatstream.com>
2025-12-01 17:39:23 +0100

    R: index

--- xo/utility/index.h
+++ xo/utility/index.h
@@ -12,32 +12,28 @@ namespace xo
 		using index_type = I;
 
 		// default, copy and move constructor
-		constexpr index_class() : idx( invalid_index_value() ) {}
+		constexpr index_class() : idx( no_value_sentinel ) {}
 		index_class( const index_class& o ) = default;
 		index_class( index_class&& o ) noexcept : idx( o.idx ) { o.reset(); }
 
 		// explicit conversion constructors
 		explicit constexpr index_class( index_type v ) : idx( v ) {}
 		template<typename U> explicit constexpr index_class( index_class<U, I> o ) : idx( o.value() ) {}
-		explicit constexpr index_class( size_t i ) : idx( i <= max_value() ? index_type( i ) : invalid_index_value() ) {}
-		explicit constexpr index_class( int i ) : idx( i >= 0 && unsigned( i ) <= max_value() ? index_type( i ) : invalid_index_value() ) {}
+		explicit constexpr index_class( size_t i ) : idx( i <= max_value() ? index_type( i ) : no_value_sentinel ) {}
+		explicit constexpr index_class( int i ) : idx( i >= 0 && unsigned( i ) <= max_value() ? index_type( i ) : no_value_sentinel ) {}
 
 		// default copy and move assignment
 		index_class& operator=( const index_class& o ) { idx = o.idx; return *this; }
 		index_class& operator=( index_class&& o ) noexcept { idx = o.idx; o.reset(); return *this; }
 
 		index_type value() const { return idx; }
-		index_type max_value() const { return invalid_index_value() - 1; }
-		explicit operator bool() const { return idx != invalid_index_value(); }
+		bool has_value() const { return idx != no_value_sentinel; }
+		explicit operator bool() const { return has_value(); }
 
-		void reset() { idx = invalid_index_value(); }
+		void reset() { idx = no_value_sentinel; }
 
 		template< typename C > auto iter( const C& c ) const { return c.begin() + idx; }
 		template< typename C > auto iter( C& c ) const { return c.begin() + idx; }
-
-		static constexpr index_class zero() { return index_class( index_type( 0 ) ); }
-		template< typename C > static index_class end( const C& c ) { return index_class( index_type( c.size() ) ); }
-		template< typename C > static index_class back( const C& c ) { return index_class( index_type( c.size() ) - 1 ); }
 
 		index_class operator+( index_type ofs ) const { return index_class( idx + ofs ); }
 		index_class operator-( index_type ofs ) const { return index_class( idx - ofs ); }
@@ -53,10 +49,16 @@ namespace xo
 		friend bool operator==( const index_class a, const index_class b ) { return a.idx == b.idx; }
 		friend bool operator!=( const index_class a, const index_class b ) { return a.idx != b.idx; }
 
+		template< typename C > static index_class end( const C& c ) { return index_class( index_type( c.size() ) ); }
+		template< typename C > static index_class back( const C& c ) { return index_class( index_type( c.size() ) - 1 ); }
+
+		static constexpr index_class zero() { return index_class( index_type( 0 ) ); }
+		static constexpr index_type max_value() { return no_value_sentinel - 1; }
+
 		index_type idx;
 
 	protected:
-		static constexpr index_type invalid_index_value() { return ~index_type( 0 ); }
+		static constexpr index_type no_value_sentinel = ~index_type( 0 );
 	};
 
 	/// strongly typed index span
