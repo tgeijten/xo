cb958b7f6b488d7dc7a77f3777a2f6a38be6ebf0
Thomas Geijtenbeek <thomas@goatstream.com>
2026-04-30 17:00:51 +0200

    N: distance_point_line() (needs testing); R: geometry_algorithms.h

--- xo/geometry/geometry_algorithms.h
+++ xo/geometry/geometry_algorithms.h
@@ -6,32 +6,38 @@
 namespace xo
 {
 	template< typename T >
-	vec3_<T> closest_point_line( const vec3_<T>& p1, const vec3_<T>& p2, const vec3_<T>& q2 ) {
-		auto ld = q2 - p2;
-		auto t = dot_product( p1 - p2, ld ) / squared_length( ld );
-		return p2 + t * ld;
+	vec3_<T> closest_point_line( const vec3_<T>& p, const vec3_<T>& l1, const vec3_<T>& l2 ) {
+		auto ld = l2 - l1;
+		auto t = dot_product( p - l1, ld ) / squared_length( ld );
+		return l1 + t * ld;
 	}
 	
 	template< typename T >
-	vec3_<T> closest_point_segment( const vec3_<T>& p1, const vec3_<T>& p2, const vec3_<T>& q2 ) {
-		auto ld = q2 - p2;
-		auto t = clamped( dot_product( p1 - p2, ld ) / squared_length( ld ), T( 0 ), T( 1 ) );
-		return p2 + t * ld;
+	T distance_point_line( const vec3_<T>& p, const vec3_<T>& l1, const vec3_<T>& l2 ) {
+		auto ld = l2 - l1;
+		return length( cross_product( p - l1, ld ) ) / length( ld );
+	}
+	
+	template< typename T >
+	vec3_<T> closest_point_segment( const vec3_<T>& p, const vec3_<T>& s1, const vec3_<T>& s2 ) {
+		auto ld = s2 - s1;
+		auto t = clamped( dot_product( p - s1, ld ) / squared_length( ld ), T( 0 ), T( 1 ) );
+		return s1 + t * ld;
 	}
 
 	template< typename T >
-	pair<vec3_<T>, vec3_<T>> closest_line_line( const vec3_<T>& p1, const vec3_<T>& p2, const vec3_<T>& q1, const vec3_<T>& q2 ) {
-		vec3_<T> pdir = p2 - p1;
-		vec3_<T> qdir = q2 - q1;
-		vec3_<T> p1q1 = p1 - q1;
-		T pdot = squared_length( pdir );
-		T qdot = squared_length( qdir );
-		T pqdot = dot_product( pdir, qdir );
-		T ppqdot = dot_product( pdir, p1q1 );
-		T qpqdot = dot_product( qdir, p1q1 );
-		T denom = pdot * qdot - pqdot * pqdot;
-		T pt = ( denom != T( 0 ) ) ? ( pqdot * qpqdot - ppqdot * qdot ) / ( denom ) : T( 0 );
-		T qt = ( pqdot * pt + qpqdot ) / qdot;
-		return { p1 + pdir * pt, q1 + qdir * qt };
+	pair<vec3_<T>, vec3_<T>> closest_line_line( const vec3_<T>& l1, const vec3_<T>& l2, const vec3_<T>& m1, const vec3_<T>& m2 ) {
+		vec3_<T> ldir = l2 - l1;
+		vec3_<T> mdir = m2 - m1;
+		vec3_<T> l1m1 = l1 - m1;
+		T ldot = squared_length( ldir );
+		T mdot = squared_length( mdir );
+		T lmdot = dot_product( ldir, mdir );
+		T llmdot = dot_product( ldir, l1m1 );
+		T mlmdot = dot_product( mdir, l1m1 );
+		T denom = ldot * mdot - lmdot * lmdot;
+		T tl = ( denom != T( 0 ) ) ? ( lmdot * mlmdot - llmdot * mdot ) / ( denom ) : T( 0 );
+		T tm = ( lmdot * tl + mlmdot ) / mdot;
+		return { l1 + ldir * tl, m1 + mdir * tm };
 	}
 }
